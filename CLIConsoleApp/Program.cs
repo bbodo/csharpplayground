a94e444e2b36b7a5b87ea9925cfe6235a328718c
bobrae <braegger.bodo@gmail.com>
2018-07-03 13:57:05 +0200

    remove duplicated / unnecessary code bit

--- CLIConsoleApp/Program.cs
+++ CLIConsoleApp/Program.cs
@@ -12,18 +12,6 @@ namespace CLIConsoleApp
             List<string> a = new List<string>();
             int i = 0;
 
-            foreach(var q in s.ToCharArray())
-            {
-                a.Add(q.ToString());
-                i++;
-                if (q.ToString() == "a" | q.ToString() == "o" | q.ToString() == "i")
-                {
-                    for(int j = i + 1; j < 5; j++)
-                    {
-                        a.Add(q.ToString());
-                    }
-                }
-            }
             string[] b = { };
             InputSlowmotionizer(b);
         }
