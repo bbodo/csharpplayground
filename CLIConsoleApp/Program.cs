b91568ded46955e199c6fef16d1e3cb12979ba80
bobrae <braegger.bodo@gmail.com>
2018-07-03 14:15:40 +0200

    Add example programs for yield and exception statements

--- CLIConsoleApp/Program.cs
+++ CLIConsoleApp/Program.cs
@@ -14,7 +14,19 @@ namespace CLIConsoleApp
 
             string[] b = { };
             InputSlowmotionizer(b);
+
+            YieldStatement(b);
+            Range(-10, 10);
+            string[] c = { "1", "10" };
+            TryCatch(c);
+            string[] d = { "1" };
+            TryCatch(d);
+            string[] e = { "1", "0" };
+            TryCatch(e);
+            CheckedUnchecked(b);
+
         }
+
 
         static void InputSlowmotionizer(string[] args)
         {
@@ -45,6 +57,67 @@ namespace CLIConsoleApp
             }
             return string.Join("", a.ToArray());
         }
+
+
+        static void YieldStatement(string[] args)
+        {
+            foreach (int i in Range(-10, 10))
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static IEnumerable<int> Range(int from, int to)
+        {
+            for (int i = from; i < to; i++)
+            {
+                yield return i;
+            }
+            yield break;
+        }
+
+        static double Divide(double x, double y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException();
+            return x / y;
+        }
+        static void TryCatch(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    throw new InvalidOperationException("Two numbers required");
+                }
+                double x = double.Parse(args[0]);
+                double y = double.Parse(args[1]);
+                Console.WriteLine(Divide(x, y));
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+
+
+        static void CheckedUnchecked(string[] args)
+        {
+            int x = int.MaxValue;
+
+            checked
+            {
+                Console.WriteLine(x + 1);  // Exception
+            }
+            unchecked
+            {
+                Console.WriteLine(x + 1);  // Overflow
+            }
+        }
+
 
     }
 }
