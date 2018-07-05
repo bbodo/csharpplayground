28a5ce34a673e35d91706bdeb87dfcaabfa26b98
bobrae <braegger.bodo@gmail.com>
2018-07-05 16:57:05 +0200

    Reflect on- and verbosely write what's happening

--- CLIConsoleApp/Program.cs
+++ CLIConsoleApp/Program.cs
@@ -8,22 +8,40 @@ namespace CLIConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string s = "Testing DoStatement()";
             List<string> a = new List<string>();
-            int i = 0;
 
             string[] b = { };
+            Console.WriteLine("Input for Slowmotionizer (Empty to continue): ");
             InputSlowmotionizer(b);
 
+            Console.WriteLine("ForEach Loop over Range, which yields for every i from to ");
             YieldStatement(b);
-            Range(-10, 10);
+            // Range(-10, 10);
+
+            Console.WriteLine("\nTry and Catch division exception: 1 / 10 = no error->finally goodbye");
             string[] c = { "1", "10" };
             TryCatch(c);
+            Console.WriteLine("Try and Catch division exception: 1 / ??? = too few args in string[] arg, caught->goodbye");
             string[] d = { "1" };
             TryCatch(d);
+            Console.WriteLine("Try and Catch division exception: 1 / 0 = checked 0 division, uncaught");
             string[] e = { "1", "0" };
-            TryCatch(e);
-            CheckedUnchecked(b);
+            string confirm = "Do you want to divide by zero now? (checked, no way to catch)";
+            Console.WriteLine(confirm);
+            confirm = Console.ReadLine();
+            if(confirm == "yes" || confirm == "y")
+            {
+                TryCatch(e);
+            }
+            Console.WriteLine("\nNow: the unchecked results of accessing int.MaxValue + 1? (No crash, client should handle)");
+            Unchecked();
+            string confirm1 = "Do you want to crash the program using a checked exception now? (client can't handle, like dividing by 0)";
+            Console.WriteLine(confirm1);
+            confirm1 = Console.ReadLine();
+            if(confirm1 == "yes" || confirm1 == "y")
+            {
+                Checked(); 
+            }
 
         }
 
@@ -63,7 +81,7 @@ namespace CLIConsoleApp
         {
             foreach (int i in Range(-10, 10))
             {
-                Console.WriteLine(i);
+                Console.Write($"{ i}, ");
             }
         }
         static IEnumerable<int> Range(int from, int to)
@@ -101,32 +119,25 @@ namespace CLIConsoleApp
             {
                 Console.WriteLine("Good bye!");
             }
-            string a = "a";
-            string b = "b";
-            string c = "c";
-
-            Console.WriteLine("{0},{1},{2}", a, b, c);
-            Console.WriteLine($"{a}, {b}, {c}");
         }
 
 
-        static void CheckedUnchecked(string[] args)
+        static void Unchecked()
         {
             int x = int.MaxValue;
-
-            checked
-            {
-                Console.WriteLine(x + 1);  // Exception
-            }
             unchecked
             {
                 Console.WriteLine(x + 1);  // Overflow
             }
-
-
         }
-
-       
+        static void Checked()
+        {
+            int x = int.MaxValue;
+            checked
+            {
+                Console.WriteLine(x + 1);  // Exception
+            }
+        }
 
 
     }
