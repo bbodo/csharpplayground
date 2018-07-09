be5066ab6d986fbfde0e538385cbad68a279a478
bobrae <braegger.bodo@gmail.com>
2018-07-09 13:02:32 +0200

    Add Prime Check and XUnit tests for it

---
+++ PrimeService/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < candidate/2; i++)
+            {
+                if(candidate % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
