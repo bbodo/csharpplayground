d4345fd306b2aac1c75121ce5403e1b0871aa753
bobrae <braegger.bodo@gmail.com>
2018-07-17 15:25:56 +0200

    Add new WebApp based on MVC to hold a Movie DB

---
+++ MVCWebApp/Data/MVCWebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCWebApp.Models
+{
+    public class MVCWebAppContext : DbContext
+    {
+        public MVCWebAppContext (DbContextOptions<MVCWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCWebApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MVCWebApp/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCWebApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        [Column(TypeName="decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
